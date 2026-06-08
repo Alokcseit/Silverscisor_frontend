881e39e09d050d4b590f4a05b879e7b0bdcad3e1
Alokcseit <swainalokkumar25@gmail.com>
2026-06-08 08:42:35 +0530

    fix(auth): map user data fields before signup request
    Transform the `userData` object to match the expected API schema by mapping `name` to `username` and explicitly selecting required fields before sending the POST request to `/api/auth/signup`.

--- src/components/auth/services/signup.js
+++ src/components/auth/services/signup.js
@@ -4,9 +4,16 @@ import api from '../../services/app'
 export const signupAPI = async (userData) => {
   console.log(userData);
   delete userData.confirmPassword;
+  let newUserData={
+    username: userData.name,
+    email: userData.email,
+    phone: userData.phone,
+    password: userData.password,
+    userType: userData.userType
+  }
   try {
     // URL check kar lena: /api/Signup hai ya /api/Register
-    const response = await api.post('/api/auth/signup', userData);
+    const response = await api.post('/api/auth/signup', newUserData);
 
     console.log('✅ Signup successful:', response.data);
     return response.data;
